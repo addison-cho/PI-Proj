d72f73c88d980edfc5304eda2a46363b11cff192
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-09-01 12:43:01 -0500

    starting diplay so bavya can copy and paste code from previous program

---
+++ display.java
@@ -0,0 +1,40 @@
+import javax.swing.JFrame;
+import java.awt.*;
+//imports
+
+Class display{
+    public static void main (String[]args){
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
