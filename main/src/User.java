2f148daf915242e8a1bc6f4021c62856a8d3c60c
addison-cho <111458978+addison-cho@users.noreply.github.com>
2023-09-05 13:29:30 -0500

    .

--- main/src/User.java
+++ main/src/User.java
@@ -1,9 +1,25 @@
 
 // manages interactions with the user
+import java.net.SocketPermission;
+import java.util.Scanner;
 
 public class User {
+    Scanner scan = new Scanner(System.in);
 
     public static void main (String[] args) {
-        System.out.println("test");
+        menu();
+    }
+
+    public static void menu() {
+        System.out.println("""-----------------------------
+        1) View existing curves
+        2) Edit existing curves
+        3) Add curve
+        4) Remove curve
+        5) Display
+        6) Exit
+
+        -----------------------------
+        """);
     }
 }
