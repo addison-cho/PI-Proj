4d6ae9240afd91f8c7ad5d37c822615e674d711a
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2023-09-08 12:57:47 -0500

    saved square and shapes parent class

---
+++ Shapes/Shapes/shapes_parent.java
@@ -0,0 +1,11 @@
+package Shapes;
+
+public class shapes_parent {
+
+    private String name = " ";
+    
+    public shapes_parent(String name){
+        this.name = name;
+    }
+    
+}
---
+++ Shapes/Shapes/square.java
@@ -0,0 +1,13 @@
+package Shapes;
+
+public class square extends shapes_parent {
+
+    private int length = 0; 
+    private String name = " "; 
+
+    public square(String name, int length) {
+        super(name);
+        
+    }
+    
+}
